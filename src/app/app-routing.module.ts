dcb0171ef43b0798cc5600041d78d859d2ffc587
Maeseff <ig.selman@duocuc.cl>
2021-09-20 17:55:41 -0300

    Avance 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },  {
+    path: 'welcome',
+    loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
   },
+
 ];
 
 @NgModule({
