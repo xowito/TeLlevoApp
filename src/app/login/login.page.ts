e1fa66e542df03b93cfe3375e91704ef78b3d972
xowito <xowito01@agmail.com>
2021-11-07 00:45:27 -0300

    ???

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -13,7 +13,6 @@ import { pathToFileURL } from 'url';
 export class LoginPage implements OnInit {
   formLogin : FormGroup;
 
-<<<<<<< HEAD
   @ViewChild ('username') user;
   @ViewChild ('password') password;
   @ViewChild ('mensaje') mensaje;
@@ -32,15 +31,6 @@ export class LoginPage implements OnInit {
 
     if(usuario.User == f.User && usuario.Password1 == f.Password){
       localStorage.setItem('ingresado','true')
-=======
-  @ViewChild('username') user;
-  @ViewChild('password') password;
-  @ViewChild('mensaje') mensaje;
-  constructor(private router: Router, private alertCtrl: AlertController) { }
-
-  login() {
-    if (this.user.value == "tellevo" && this.password.value == "meayudas") {
->>>>>>> 836ab5e3f0b93afa22a61ce27a6556acc28ca3e8
       this.router.navigate(['../welcome']);
     }else{
       const alert = await this.alertController.create({
