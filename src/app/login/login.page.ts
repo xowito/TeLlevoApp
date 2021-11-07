5f2a8544b67153dca42d85042c4c0a26ac182ce6
Maeseff <ig.selman@duocuc.cl>
2021-11-06 23:33:18 -0300

    formato

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -10,13 +10,13 @@ import { AlertController } from '@ionic/angular';
 
 export class LoginPage implements OnInit {
 
-  @ViewChild ('username') user;
-  @ViewChild ('password') password;
-  @ViewChild ('mensaje') mensaje;
+  @ViewChild('username') user;
+  @ViewChild('password') password;
+  @ViewChild('mensaje') mensaje;
   constructor(private router: Router, private alertCtrl: AlertController) { }
 
-  login(){
-    if(this.user.value == "tellevo" && this.password.value == "meayudas"){
+  login() {
+    if (this.user.value == "tellevo" && this.password.value == "meayudas") {
       this.router.navigate(['../welcome']);
     } else {
       this.mensaje.value = "❗ Usuario o contraseña incorrectos";
