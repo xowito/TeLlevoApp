dc5662c1e3d0f2190784eecd159dfd44de3af584
xowito <xowito01@agmail.com>
2021-11-06 23:25:55 -0300

    Arreglado

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -10,17 +11,43 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 export class RegisterPage implements OnInit {
   formRegistro:FormGroup;
 
-  constructor(private router: Router, public fb: FormBuilder) {
+  constructor(private Router: Router, private fb: FormBuilder, private alertController: AlertController) {
     this.formRegistro = this.fb.group({
-      'User': new FormControl("")
+      'User': new FormControl("",Validators.required),
+      'Correo': new FormControl("",Validators.required),
+      'Password1': new FormControl("",Validators.required),
+      'Password2': new FormControl("",Validators.required),
     })
    }
 
   ngOnInit() {
   }
 
-  login(){
-    this.router.navigate(['../login']);
-  }
 
+  async guardar(){
+    var f = this.formRegistro.value;
+
+    if(this.formRegistro.invalid){
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'Alert',
+        subHeader: 'Subtitle',
+        message: 'Rellena todos los campos antes de registrarte',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+      return;
+
+    }
+
+    var usuario = {
+      User: f.User,
+      Correo: f.Correo,
+      Password1: f.Password1,
+      Password2: f.Password2
+    }
+
+    localStorage.setItem('usuario',JSON.stringify(usuario))
+  }
 }
